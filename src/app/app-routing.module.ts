e657a23c55fb2a33beee1f9a77cf60bc1fedda17
Aymeric Dominique <aymeric.dominique@gmail.com>
2020-08-05 00:31:50 +0200

    block all url if not connected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddClassesComponent } from './add-classes/add-classes.component';
+import { AuthGuardService as AuthGuard } from './auth-guard.service';
 import { ListClassesComponent } from './list-classes/list-classes.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: ListClassesComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'add-classes',
     component: AddClassesComponent,
+    canActivate: [AuthGuard],
   },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
